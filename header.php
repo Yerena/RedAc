e56ea7eca84b5f15cef494fb53cbcb049e71c32e
Yerena <alejo.yerena@gmail.com>
2015-07-21 21:20:54 -0500

    otro commit

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/header_style.css"/>
+	</head>
+	<body>
+        <IMG SRC = "../img/banner-01.png" WIDTH="100%">
+         <IMG SRC = "img/banner-01.png" WIDTH="100%">
+        		
+	</body>
+</html>
+
